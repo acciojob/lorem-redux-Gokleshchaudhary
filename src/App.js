35671fdf0c30a030e7772b5abf15afb384f6c757
gokleshchaudhary <123gopalchaudhary123@gmail.com>
2025-08-18 16:13:29 +0530

    update file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
-import LoremContent from "./components/LoremContent";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts(data);
+        setLoading(false);
+      });
+  }, []);
+
   return (
-    <div className="app">
-      <h1>Lorem Redux App</h1>
-      <LoremContent />
+    <div className="App">
+      <h1>A short Naration of Lorem Ipsum</h1>
+
+      {loading ? (
+        <h4>Loading...</h4>
+      ) : (
+        
+        <ul className="post-list">
+          {posts.map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
